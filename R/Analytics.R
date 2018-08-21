6af232443935784c9a060a147b211b74d325afd6
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-08-21 10:39:11 -0400

    renamed

--- R/Analytics.R
+++ R/Analytics.R
@@ -1,121 +1,97 @@
-#' Play a short sound from Analytics (group)
-#' 
-#' \code{Analytics} plays a short sound which is useful if you want to get notified, 
-#' for example, when a script has finished. As an added bonus there are a number
-#' of different sounds to choose from.
+#' Play a short sound from analytics (group)
 #'
-#' If \code{Analytics} is not able to play the sound a warning is issued rather than 
-#' an error. This is in order to not risk aborting or stopping the process that
-#' you wanted to get notified about.
+#' \code{analytics} plays a short sound which is useful if you want to get
+#'   notified, for example, when a script has finished. As an added bonus there
+#'   are a number of different sounds to choose from.
 #'
-#' @param sound character string or number specifying what sound to be played by 
-#'   either specifying one of the built in sounds, specifying the path to a wav 
-#'   file or specifying an url. The default is 1. Possible sounds are:
+#' If \code{analytics} is not able to play the sound a warning is issued rather
+#'   than an error. This is in order to not risk aborting or stopping the
+#'   process that you wanted to get notified about.
+#'
+#' @param sound character string or number specifying what sound to be played
+#'   by either specifying one of the built in sounds, specifying the path to a
+#'   wav  file or specifying an url. The default is 1. Possible sounds are:
 #'   \enumerate{ \item \code{"all_done"} }
 #'   If \code{sound} does not match any of the sounds above, or is a valid path
-#'   or url, a random sound will be played. Currently \code{Analytics} can only handle
-#'   http urls, https is not supported.
+#'   or url, a random sound will be played. Currently \code{analytics} can only
+#'   handle http urls, https is not supported.
 #' @param expr An optional expression to be excecuted before the sound.
-#'
 #' @return NULL
-#'
 #' @examples
 #' # Play an "all_done" sound
-#' Analytics()
-#' 
+#' analytics()
+#'
 #' \dontrun{
 #' # Play "all_done" instead of a "all_done".
-#' Analytics("all_done")
+#' analytics("all_done")
 #' # or
-#' Analytics(1)
-#' 
+#' analytics(1)
+#'
 #' # Play a random sound
-#' Analytics(0)
-#' 
+#' analytics(0)
+#'
 #' # Update all packages and "ping" when it's ready
-#' update.packages(ask=FALSE); Analytics()
+#' update.packages(ask=FALSE); analytics()
 #' }
-#' @export
-#' @import audio
-#' @importFrom stringr str_detect
 #' @importFrom utils download.file
-
-Analytics <- function(sound = 1, expr = NULL) {
-  
+#' @export
+analytics <- function(sound = 1, expr = NULL) {
   expr
   sounds <- c(
     all_done = "All_Done.wav"
   )
-  
   sound_path <- NULL
-  if(is.na(sounds[sound]) || length(sounds[sound]) != 1) {
-    if(is.character(sound)) {
+  if (is.na(sounds[sound]) || length(sounds[sound]) != 1) {
+    if (is.character(sound)) {
       sound <- trimws(sound)
-      if(file.exists(sound)) {
+      if (file.exists(sound)) {
         sound_path <- sound
-      } else if(grepl("^https://", sound)) {
+      } else if (grepl("^https://", sound)) {
         warning("Can't currently use https urls, only http.")
-      } else if(grepl("^http://", sound)) {
-        temp_file <- tempfile(pattern="")
-        if(download.file(sound, destfile = temp_file, quiet = TRUE) == 0) { # The file was successfully downloaded
+      } else if (grepl("^http://", sound)) {
+        temp_file <- tempfile(pattern = "")
+        if (download.file(sound, destfile = temp_file, quiet = TRUE) == 0) {
           sound_path <- temp_file
         } else {
-          warning(paste("Tried but could not download", sound))
+          warning(
+            paste("Tried but could not download", sound))
         }
       } else {
-        warning(paste('"', sound, '" is not a valid sound nor path, playing a random sound instead.', sep = ""))
+        warning(
+          paste(
+            '"'
+            , sound
+            , '" is not a valid sound nor path,'
+            , 'playing a random sound instead.'
+            , sep = ""
+            )
+          )
       }
     }
   } else {
-    sound_path <- system.file(paste("sounds/AFMS/", sounds[sound], sep=""), package="geekr")
+    sound_path <- system.file(
+      paste(
+        "sounds/AFMS/"
+        , sounds[sound]
+        , sep = ""
+        )
+      , package = "geekr"
+      )
   }
-  
-  if(is.null(sound_path)) { # play a random sound
-    sound_path <- system.file(paste("sounds/AFMS/", sample(sounds, size=1), sep=""), package="geekr")
+  if (is.null(sound_path)) { # play a random sound
+    sound_path <- system.file(
+      paste(
+        "sounds/AFMS/"
+        , sample(sounds, size = 1)
+        , sep = ""
+        )
+      , package = "geekr"
+      )
   }
-  
   tryCatch(play_file(sound_path), error = function(ex) {
-    warning("Analytics() could not play the sound due to the following error:\n", ex)
+    warning(
+      "analytics() could not play the sound due to the following error:\n
+      ", ex
+      )
   })
-}
-
-is_wav_fname <- function(fname) {
-  str_detect(fname, "\\.wav$")
-}
-
-play_vlc <- function(fname) {
-  system(paste("vlc -Idummy --no-loop --no-repeat --playlist-autostart --no-media-library --play-and-exit", fname), 
-         ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
-  invisible(NULL)
-}
-
-play_paplay <- function(fname) {
-  system(paste("paplay ", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
-  invisible(NULL)
-}
-
-play_aplay <- function(fname) {
-  system(paste("aplay --buffer-time=48000 -N -q", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
-  invisible(NULL)
-}
-
-play_audio <- function(fname) {
-  sfx <- load.wave(fname)
-  play(sfx)
-}
-
-play_file <- function(fname) {
-  if(Sys.info()["sysname"] == "Linux") {
-    if(is_wav_fname(fname) && nchar(Sys.which("paplay")) >= 1) {
-      play_paplay(fname)
-    } else if(is_wav_fname(fname) && nchar(Sys.which("aplay")) >= 1) {
-      play_aplay(fname)
-    } else if(nchar(Sys.which("vlc")) >= 1) {
-      play_vlc(fname)
-    } else {
-      play_audio(fname)
-    }
-  } else {
-    play_audio(fname)
-  }
 }

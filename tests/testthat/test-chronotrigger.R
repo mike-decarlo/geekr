227ff9fc3fdc3b6075c2b501628ebed9187631ff
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-08-21 09:17:52 -0400

    removed http for now

--- tests/testthat/test-chronotrigger.R
+++ tests/testthat/test-chronotrigger.R
@@ -17,15 +17,6 @@ test_that("https/http", {
     , "Can't currently use https urls, only http."
   )
   Sys.sleep(5)
-  expect_equal(
-    ChronoTrigger(
-      sound = "http://themushroomkingdom.net/sounds/wav/smw/smw_1-up.wav"
-    )
-    , SuperMario(
-      sound = "http://themushroomkingdom.net/sounds/wav/smw/smw_1-up.wav"
-    )
-  )
-  Sys.sleep(5)
 })
 
 test_that("sound from sounds works", {

ed9bd5b4fd5c7f4b5abe911ce06cb006909ebffc
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-08-21 07:53:00 -0400

    more tests

--- tests/testthat/test-supermario.R
+++ tests/testthat/test-supermario.R
@@ -7,3 +7,12 @@ test_that("no errors works", {
     , '"F" is not a valid sound nor path, playing a random sound instead.'
     )
 })
+
+test_that("https returns warning", {
+  expect_warning(
+    SuperMario(
+      sound = "https://themushroomkingdom.net/sounds/wav/smw/smw_1-up.wav"
+    )
+    , "Can't currently use https urls, only http."
+  )
+})
--- tests/testthat/test-warcraft.R
+++ tests/testthat/test-warcraft.R
@@ -5,5 +5,14 @@ test_that("no errors works", {
   expect_warning(
     Warcraft(sound = "F")
     , '"F" is not a valid sound nor path, playing a random sound instead.'
-  )
-})
+    )
+  })
+
+test_that("https returns warning", {
+    expect_warning(
+      Warcraft(
+        sound = "https://themushroomkingdom.net/sounds/wav/smw/smw_1-up.wav"
+      )
+      , "Can't currently use https urls, only http."
+    )
+  })
